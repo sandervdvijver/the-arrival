9e0dd4ed1c53dfb9b7f1cf8576b8ab362e26c152
Van de Vyver Sander <vyvers@FVFG153LQ05F.local>
2022-09-28 15:25:44 +0200

    whatever

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,40 @@
 <script>
-	export let name;
+	let locations = ["in a peaceful valley", "in an anonymous suburb", "at a crossroads", "at a remote outpost", "in a fishing village", "at a train station", "in a busy inn", "at the grounds of an enormous factory", "at a border crossing", "at a hut in the forest", "at an old stone cabin in the mountains", "in a solemn courtyard", "at a modernist mansion overlooking the city", "in a crowded harbour", "at an ancient tree", "in a quiet provincial town", "at a market square", "in a hotel past its glory days", "in a roadside restaurant", "in a dusty bookstore"];
+	let atmospheres = ["on an overcast day", "in the middle of a starless night", "during summer solstice", "in the fog", "in winter", "during first snow", "in the early morning", "in autumn rain", "on a particularly hot day", "in stormy weather", "under moonlight", "after a long absence", "at dusk", "weary from travelling", "cast in shadows", "shrouded in smoke", "lit by a single light source"];
+	let characters = ["a giant arrives", "a stranger arrives", "a large bird arrives", "a band of outsiders arrives", "a peculiar child arrives", "a cloaked figure arrives", "two travellers arrive", "a hurried messenger arrive", "a private investigator arrives", "a hunter arrives", "a large animal arrives", "a family of four arrives", "an adolescent arrives", "a travelling saleswoman arrives", "a travelling salesman arrives", "an unwelcome guest arrives", "an outlaw arrives", "a bearer of bad news arrives"];
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+
+	function pickItem(list,el) {
+		var chosenNumber = Math.floor(Math.random() * list.length);
+    	el = list[chosenNumber];
+    	return el;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<ul>
+		<li>{pickItem[locations]}</li>
+		<li>{pickItem[atmospheres]}</li>
+		<li>{pickItem[characters]}</li>
+	</ul>
+	<button on:click={handleClick}>Refresh</button>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+    	width: 50vw;
+    	margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	ul {
+		padding-left: 0;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	li {
+		list-style-type: none;
+		padding: 0;
+		margin: 0;
 	}
 </style>
